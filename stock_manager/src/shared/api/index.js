7c80532b17ae84a0b12612141dba47bf7a1026b9
Fer Enoch <ferenochsoames@gmail.com>
2023-12-31 16:50:56 -0300

    Cleaning repo of old api system

--- stock_manager/src/shared/api/index.js
+++ stock_manager/src/shared/api/index.js
@@ -1,31 +1,14 @@
-import { firebaseDatabaseCreateFiles } from '../../features/databaseUpdate/model/firebaseDatabaseCreateFiles';
-import { firebaseDatabaseDeleteFiles } from '../../features/databaseUpdate/model/firebaseDatabaseDeleteFiles';
-import { firebaseDatabaseUpdateFiles } from '../../features/databaseUpdate/model/firebaseDatabaseUpdateFiles';
-import { updateCorporativeInfoInWebAppCache } from '../../features/databaseUpdate/model/updateCorporativeInfoWebAppCache';
-import {
-  addProductsToWebAppCache,
-  deleteProductsInWebAppCache,
-  updateProductsInWebAppCache,
-} from '../../features/databaseUpdate/model/updateProductsWebAppCache';
 import { DATABASE_OPERATIONS } from './config/database-operations';
+import { updateCorporativeInfoInWebAppCache } from '../../features/databaseUpdate/model/updateCorporativeInfoWebAppCache';
 import { createFirestoreDocument } from './lib/createFirestoreDocument';
 import { updateFirestoreDocument } from './lib/updateFirestoreDocument';
-import { noChangeDataLogger } from './model/noChangeDataLogger';
 
-const { LEAVE, CREATE, UPDATE, DELETE } = DATABASE_OPERATIONS;
-
-export const PRODUCTS_DATABASE_API_ACTIONS = {
-  [LEAVE]: noChangeDataLogger,
-  [CREATE]: firebaseDatabaseCreateFiles,
-  [UPDATE]: firebaseDatabaseUpdateFiles,
-  [DELETE]: firebaseDatabaseDeleteFiles,
-};
-
-export const WEB_APP_API_ACTIONS = {
-  [CREATE]: addProductsToWebAppCache,
-  [UPDATE]: updateProductsInWebAppCache,
-  [DELETE]: deleteProductsInWebAppCache,
-};
+const {
+  // LEAVE,
+  CREATE,
+  UPDATE,
+  // DELETE
+} = DATABASE_OPERATIONS;
 
 /**
  *  New version of the API
