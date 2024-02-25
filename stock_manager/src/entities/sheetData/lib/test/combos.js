717111dcf389931d17addfec564276078a5d7577
Fer Enoch <ferenochsoames@gmail.com>
2024-02-25 20:51:03 -0300

    Add data from cache-sheet to debug updating combos route

--- stock_manager/src/entities/sheetData/lib/test/combos.js
+++ stock_manager/src/entities/sheetData/lib/test/combos.js
@@ -94,3 +94,12 @@ export const testCombosMapBuilding = {
     },
   ],
 };
+
+export const testCombosCache = [
+  {
+    category: 'Promos del finde',
+    'firestoreName-ID': 'WdKdPfYyQAk2DlbNJHCy',
+    firestoreName: 'projects/stock-backend-392114/databases/(default)/documents/combos-test/WdKdPfYyQAk2DlbNJHCy',
+    data: '[{"id":"*001","isCombo":true,"category":"Promos del finde","type":"Promo verano","featInfo":"Solo por febrero!","products":[["#003","2"],["#055","2"],["#157","1"]],"price":10500,"imageID":"18m_BDX4spR9F2ZL8gBEujmEBRKP2fywF","imageURL":"https://drive.google.com/uc?export=view&id=18m_BDX4spR9F2ZL8gBEujmEBRKP2fywF"},{"id":"*002","isCombo":true,"category":"Promos del finde","type":"Promo invierno","featInfo":"Solo por mayo","products":[["#003","2"],["#055","2"],["#157","1"]],"price":15060,"imageID":"18m_BDX4spR9F2ZL8gBEujmEBRKP2fywF","imageURL":"https://drive.google.com/uc?export=view&id=18m_BDX4spR9F2ZL8gBEujmEBRKP2fywF"}]',
+  },
+];
