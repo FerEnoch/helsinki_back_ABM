df2fdeca454984bfc1cadb012da5ead7b3d6887b
Fer Enoch <ferenochsoames@gmail.com>
2024-03-11 18:31:16 -0300

    Add staging mode web app url pointing to prod-testing branch deployment.

--- stock_manager/src/features/databaseUpdate/config/api.js
+++ stock_manager/src/features/databaseUpdate/config/api.js
@@ -11,10 +11,10 @@ const stagingMode = process.env.STAGING;
 // };
 
 export const LOCAL_PRODUCTION_URL = 'https://sjq5dd8h-3000.brs.devtunnels.ms';
-// const HOSTING_TESTING_URL = 'https://helsinki-delivery-web-app-git-prod-testing-ferenoch.vercel.app';
+export const HOSTING_TESTING_URL = 'https://helsinki-delivery-web-app-git-prod-testing-ferenoch.vercel.app';
 export const HOSTING_URL = 'https://www.helsinkidelivery.com.ar';
 
-const prodModesURLs = stagingMode ? LOCAL_PRODUCTION_URL : HOSTING_URL;
+const prodModesURLs = stagingMode ? HOSTING_TESTING_URL : HOSTING_URL;
 
 export const PRODUCTION_URL = devMode ? LOCAL_PRODUCTION_URL : prodModesURLs;
 
